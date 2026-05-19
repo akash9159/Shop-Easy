76147fcb1d61dcec5243c9f451aa4d14af7c96df
Akash <akash@gmail.com>
2026-05-19 20:35:13 +0530

    app js file message changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the akash application!');
+    res.send('Welcome to the akash application from akash clazie !');
 });
 
 app.listen(port, () => {
